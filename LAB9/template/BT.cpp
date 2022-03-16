5ac2700e89054470a775f94c136bfb1009f441be
HoChuanYu <44057809+HoChuanYu@users.noreply.github.com>
2022-03-16 23:58:37 +0800

    Create BT.cpp

---
+++ LAB9/template/BT.cpp
@@ -0,0 +1,12 @@
+#include "BT.h"
+#include <vector>
+#include <queue>
+using namespace std;
+void BT::insert(Node* insertNode) {
+	//implement the insert function here
+}
+
+
+Node* BT::getRoot() {
+	return root;
+}
